4eb69c3ac36289b8f693712b159870c8c714b43b
hernan-clich <cervece41@gmail.com>
2020-10-08 23:15:32 -0300

    Adjusted alignment of single benefits component

--- src/styles/Home/SingleBenefit.js
+++ src/styles/Home/SingleBenefit.js
@@ -1,49 +1,55 @@
 import styled from 'styled-components';
 
 export const StyledBenefit = styled.div`
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: flex-start;
-    align-items: center;
     margin: 0 1em 3em;
     min-width: 10vw;
     max-width: 15vw;
 
-    .sing-ben-img {
-        min-height: 120px;
-    }
-
-    .sing-ben-text {
+    #tech-wrapper {
         display: flex;
         flex-flow: column nowrap;
+        justify-content: flex-start;
+        align-items: center;
 
-        span {
-            text-align: center;
-            margin-top: 1em;
-            font-family: Montserrat-Medium;
-            font-size: clamp(1rem, 1.5rem, 1.5vw);
-            margin: 0;
+        .sing-ben-img {
+            min-height: 120px;
+        }
+
+        .sing-ben-text {
+            display: flex;
+            flex-flow: column nowrap;
+
+            span {
+                text-align: center;
+                margin-top: 1em;
+                font-family: Montserrat-Medium;
+                font-size: clamp(1rem, 1.5rem, 1.5vw);
+                margin: 0;
+            }
         }
     }
+    
 
     @media (min-width: 1361px){
         margin: 0 1em 3em;
         min-width: 10vw;
         max-width: 15vw;
 
-        .sing-ben-img {
-            min-height: 120px;
+        #tech-wrapper {
+            .sing-ben-img {
+                min-height: 120px;
 
-            img {
-                width: 7.5vw;
+                img {
+                    width: 7.5vw;
+                }
             }
-        }
 
-        .sing-ben-text {
+            .sing-ben-text {
 
-            span {
-                margin-top: 0.25em;
-                font-size: 1.75vw;
+                span {
+                    margin-top: 0.25em;
+                    font-size: 1.75vw;
+                }
             }
         }
     }
